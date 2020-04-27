122ddb69cbe5a0d488d2e69cdadacda74aca2592
Apple <achmadqomarudin>
2020-04-27 17:12:23 +0700

    App PushNotification-FCM

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PushNotification-FCM
 Aplikasi Push Notification dengan FCM (Firebase Cloud Messaging) untuk yg baru belajar
+
+# Source
+https://medium.com/@derangga/push-notification-menggunakan-fcm-firebase-cloud-messaging-9d2a69a43c3c
---
+++ app/src/main/java/com/example/notification/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.notification;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("NotifApps", "NotifyApps", NotificationManager.IMPORTANCE_HIGH);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        getCurrentFirebaseToken();
+
+        Bundle bundle       = getIntent().getExtras();
+        TextView textView   = findViewById(R.id.text);
+        TextView title      = findViewById(R.id.title);
+
+        if(bundle != null)  {
+            title.setText(bundle.getString("title"));
+            textView.setText(bundle.getString("message"));
+        }
+    }
+
+    private void getCurrentFirebaseToken(){
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w("TAG", "getInstanceId failed", task.getException());
+                            return;
+                        }
+
+                        // Get new Instance ID token
+                        String token = task.getResult().getToken();
+                        Log.e("currentToken", token);
+
+                        // Log and toast
+                        // Insert this token at (Add FCM Registration token)
+                        String msg = getString(R.string.msg_token_fmt, token);
+                        Log.d("TAG", msg);
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    public void onBackPressed() {
+        finishAffinity();
+    }
+}
